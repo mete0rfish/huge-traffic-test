491f0705456cd5387e7ef6cb2678fb54c6d45370
mete0rfish <sungwon326@naver.com>
2025-11-20 17:22:19 +0900

    initial commit

---
+++ huge-traffic-test/src/main/kotlin/com/mete0rfish/huge_traffic_test/HugeTrafficTestApplication.kt
@@ -0,0 +1,11 @@
+package com.mete0rfish.huge_traffic_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HugeTrafficTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<HugeTrafficTestApplication>(*args)
+}
---
+++ huge-traffic-test/src/test/kotlin/com/mete0rfish/huge_traffic_test/HugeTrafficTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mete0rfish.huge_traffic_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HugeTrafficTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
